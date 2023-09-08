5ee67a9a5c4f80c3f05f9df78523aaaf0c6d5b4e
Thomas Regourd <tom.reg@hotmail.fr>
2023-09-08 12:21:14 +0200

    updated client for moderation status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
 	const renderedComments = comments.map((comment) => {
-		return <li key={comment.id}>{comment.content}</li>;
+		let content;
+
+		if (comment.status === 'approved') {
+			content = comment.content;
+		}
+		if (comment.status === 'rejected') {
+			content = 'Rejected';
+		}
+		if (comment.status === 'pending') {
+			content = 'Pending';
+		}
+		return <li key={comment.id}>{content}</li>;
 	});
 
 	return <ul>{renderedComments}</ul>;
