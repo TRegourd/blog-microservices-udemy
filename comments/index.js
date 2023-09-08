8f5eb0e5886c3fc9bb93d42eb1513aafe5dba51b
Thomas Regourd <tom.reg@hotmail.fr>
2023-09-08 12:17:14 +0200

    moderation service ok

--- comments/index.js
+++ comments/index.js
@@ -25,7 +25,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 	const comments = commentsByPostId[postId] || [];
 
-	comments.push({ id: commentId, content });
+	comments.push({ id: commentId, content, status: 'pending' });
 
 	commentsByPostId[postId] = comments;
 
@@ -36,6 +36,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 				id: commentId,
 				content,
 				postId: postId,
+				status: 'pending',
 			},
 		})
 		.catch((err) => {
@@ -47,6 +48,31 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 app.post('/events', async (req, res) => {
 	console.log('Received event', req.body.type);
+
+	const { type, data } = req.body;
+
+	if (type === 'CommentModerated') {
+		const { id, postId, status, content } = data;
+		const comments = commentsByPostId[postId];
+		const comment = comments.find((comment) => {
+			return comment.id === id;
+		});
+		comment.status = status;
+
+		await axios
+			.post('http://localhost:4005/events', {
+				type: 'CommentUpdated',
+				data: {
+					id: id,
+					content,
+					postId,
+					status,
+				},
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
 	res.send({});
 });
